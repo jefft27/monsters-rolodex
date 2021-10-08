57358956c69557c5d54517f7c44623be177e9f40
Jeffrey Tan <jefftan274@gmail.com>
2021-10-07 19:27:43 -0700

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from './components/card-list/card-list.component'
+import { Component } from 'react'
+import { SearchBox } from './components/search-box/search-box.component'
+
+/*
+Tenets of being a great React developer
+
+1. Decide on Components
+2. Decide on the State and where it lives
+3. What changes when state changes
+*/
+
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            monsters: [],
+            searchField: '',
+        }
+
+        //this.handleChange = this.handleChange.bind(this)
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState({ monsters: users }))
+    }
+
+    // set 'this.handleChange = this.handleChange.bind(this) in the constructor
+    // ES6, arrow function automatically binds 'this' to App component underneath the hood
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value })
+    }
+
+    render() {
+        const { monsters, searchField } = this.state
+        const filteredMonsters = monsters.filter((monster) =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+
+        return (
+            <div className="App">
+                <h1>Monsters Rolodex</h1>
+                <SearchBox
+                    placeholder="Search monsters"
+                    handleChange={this.handleChange}
+                />
+                <CardList monsters={filteredMonsters} />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
